653a8316055e987dbcf7fe2d94ed0b5f3b7a9a97
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2024-11-10 01:44:37 +0000

    back

--- src/main/java/xyz/amymialee/opspotting/mixin/PlayerManagerMixin.java
+++ src/main/java/xyz/amymialee/opspotting/mixin/PlayerManagerMixin.java
@@ -5,16 +5,21 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.ChunkLoadDistanceS2CPacket;
-import net.minecraft.network.packet.s2c.play.CommonPlayerSpawnInfo;
 import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.GlobalPos;
+import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Mixin(PlayerManager.class)
@@ -27,8 +32,8 @@ public class PlayerManagerMixin {
         }
     }
 
-    @WrapOperation(method = "onPlayerConnect", at = @At(value = "NEW", target = "(IZLjava/util/Set;IIIZZZLnet/minecraft/network/packet/s2c/play/CommonPlayerSpawnInfo;Z)Lnet/minecraft/network/packet/s2c/play/GameJoinS2CPacket;"))
-    private GameJoinS2CPacket opspotting$assumemax(int playerEntityId, boolean hardcore, Set<RegistryKey<World>> dimensionIds, int maxPlayers, int viewDistance, int simulationDistance, boolean reducedDebugInfo, boolean showDeathScreen, boolean doLimitedCrafting, CommonPlayerSpawnInfo commonPlayerSpawnInfo, boolean enforcesSecureChat, @NotNull Operation<GameJoinS2CPacket> original, ClientConnection connection, @NotNull ServerPlayerEntity player) {
-        return original.call(playerEntityId, hardcore, dimensionIds, maxPlayers, player.hasPermissionLevel(4) ? 32 : viewDistance, simulationDistance, reducedDebugInfo, showDeathScreen, doLimitedCrafting, commonPlayerSpawnInfo, enforcesSecureChat);
+    @WrapOperation(method = "onPlayerConnect", at = @At(value = "NEW", target = "(IZLnet/minecraft/world/GameMode;Lnet/minecraft/world/GameMode;Ljava/util/Set;Lnet/minecraft/registry/DynamicRegistryManager$Immutable;Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/registry/RegistryKey;JIIIZZZZLjava/util/Optional;I)Lnet/minecraft/network/packet/s2c/play/GameJoinS2CPacket;"))
+    private GameJoinS2CPacket opspotting$assumemax(int playerEntityId, boolean hardcore, GameMode gameMode, @Nullable GameMode previousGameMode, Set<RegistryKey<World>> dimensionIds, DynamicRegistryManager.Immutable registryManager, RegistryKey<DimensionType> dimensionType, RegistryKey<World> dimensionId, long sha256Seed, int maxPlayers, int viewDistance, int simulationDistance, boolean reducedDebugInfo, boolean showDeathScreen, boolean debugWorld, boolean flatWorld, Optional<GlobalPos> lastDeathLocation, int portalCooldown, @NotNull Operation<GameJoinS2CPacket> original, ClientConnection connection, @NotNull ServerPlayerEntity player) {
+        return original.call(playerEntityId, hardcore, gameMode, previousGameMode, dimensionIds, registryManager, dimensionType, dimensionId, sha256Seed, maxPlayers, player.hasPermissionLevel(4) ? 32 : viewDistance, simulationDistance, reducedDebugInfo, showDeathScreen, debugWorld, flatWorld, lastDeathLocation, portalCooldown);
     }
 }
--- src/main/java/xyz/amymialee/opspotting/mixin/ServerChunkLoadingManagerMixin.java
+++ src/main/java/xyz/amymialee/opspotting/mixin/ServerChunkLoadingManagerMixin.java
@@ -2,16 +2,19 @@ package xyz.amymialee.opspotting.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerChunkLoadingManager;
+import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import net.minecraft.util.math.ChunkPos;
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(ServerChunkLoadingManager.class)
+@Mixin(ThreadedAnvilChunkStorage.class)
 public class ServerChunkLoadingManagerMixin {
-    @WrapOperation(method = "getViewDistance", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(III)I"))
-    private int opspotting$playerpreference(int value, int min, int max, @NotNull Operation<Integer> original, @NotNull ServerPlayerEntity player) {
-        return player.hasPermissionLevel(4) ? value : original.call(value, min, max);
+    @WrapOperation(method = "method_17219", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ThreadedAnvilChunkStorage;isWithinDistance(IIIII)Z", ordinal = 1))
+    private boolean opspotting$maxed(int x1, int z1, int x2, int z2, int distance, @NotNull Operation<Boolean> original, ChunkPos  chunkPos, int i, MutableObject<ChunkDataS2CPacket> mutableObject, @NotNull ServerPlayerEntity player) {
+        return original.call(x1, z1, x2, z2, player.hasPermissionLevel(4) ? 32 : distance);
     }
 }
